6d275665ee153a35503feaf0fce6728ddffa74c1
elena <kev7570@gmail.com>
2026-03-01 21:20:39 +0300

    добавлен новый функционал, изменен main.py, переписаны тесты, добавлен отчет о покрытии, проведена проверка линтерами

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,19 +1,31 @@
-import pytest
 from src.product import Product
 
 
-@pytest.fixture
-def sample_product():
-    """Подготовка экземпляра продукта для тестирования"""
-    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+def test_product_creation():
+    """
+    Тестирование создания объекта Product и его атрибутов.
+    """
+    product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    assert product.name == "Samsung Galaxy S23 Ultra"
+    assert product.description == "256GB, Серый цвет, 200MP камера"
+    assert product.price == 180000.0
+    assert product.quantity == 5
 
 
-def test_product_initialization(sample_product):
+def test_price_setter():
     """
-    Тестируем корректность инициализации объекта Product.
+    Тестирование работы сеттера для цены: проверка ограничений на установку отрицательных и нулевых цен.
     """
-    assert isinstance(sample_product, Product)
-    assert sample_product.name == "Samsung Galaxy S23 Ultra"
-    assert sample_product.description == "256GB, Серый цвет, 200MP камера"
-    assert sample_product.price == 180000.0
-    assert sample_product.quantity == 5
+    product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+    # Устанавливаем допустимую цену
+    product.price = 200000.0
+    assert product.price == 200000.0
+
+    # Пробуем установить отрицательную цену
+    product.price = -100
+    assert product.price == 200000.0  # Цена не изменилась
+
+    # Пробуем установить нулевую цену
+    product.price = 0
+    assert product.price == 200000.0  # Цена не изменилась
